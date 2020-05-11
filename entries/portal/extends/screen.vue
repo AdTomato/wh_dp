3d0ac364de7beeeddd4e47fae2a8488228d07733
Rosecaroline <1654624737@qq.com>
2020-05-11 14:30:13 +0800

    页面

--- entries/portal/extends/screen.vue
+++ entries/portal/extends/screen.vue
@@ -9,83 +9,129 @@
       <div class="screen-left">
         <!-- 天气情况 -->
         <div class="screen-item">
-          <img class="screen-item-bg" src="./assets/images/left_item_bg.png" alt />
-          <div class="screen-item-main">
-            <h3 class="screen-item-title">天气情况</h3>
-            <div class="weather-info">
-              <span>2020年4月7号</span>
-              <span>星期二</span>
-              <img src="./assets/images/sun_icon.png" alt />
-              <span>7-18℃</span>
-            </div>
-            <ul class="weather-item">
-              <li>
-                <span class="weather-item-left weather-item-mid-left">湿度</span>
-                <span class="weather-item-right weather-item-mid-right">99%</span>
-              </li>
-              <li>
-                <span class="weather-item-left weather-item-big-left">空气质量</span>
-                <span class="weather-item-right weather-item-big-right">良</span>
-              </li>
-              <li>
-                <span class="weather-item-left weather-item-small-left">气温</span>
-                <span class="weather-item-right weather-item-small-right">14.1℃</span>
-              </li>
-              <li>
-                <span class="weather-item-left weather-item-mid-left">风力</span>
-                <span class="weather-item-right weather-item-mid-right">1级</span>
-              </li>
-              <li>
-                <span class="weather-item-left weather-item-big-left">风向</span>
-                <span class="weather-item-right weather-item-big-right">东南风</span>
-              </li>
-              <li>
-                <span class="weather-item-left weather-item-small-left">能见度</span>
-                <span class="weather-item-right weather-item-small-right">9.66KM</span>
-              </li>
-            </ul>
+          <h3 class="screen-item-title">天气情况</h3>
+          <div class="weather-info">
+            <span>2020年4月7号</span>
+            <span>星期二</span>
+            <img src="./assets/images/sun_icon.png" alt />
+            <span>7-18℃</span>
           </div>
+          <ul class="weather-item">
+            <li>
+              <span class="weather-item-left weather-item-mid-left">湿度</span>
+              <span class="weather-item-right weather-item-mid-right">99%</span>
+            </li>
+            <li>
+              <span class="weather-item-left weather-item-big-left">空气质量</span>
+              <span class="weather-item-right weather-item-big-right">良</span>
+            </li>
+            <li>
+              <span class="weather-item-left weather-item-small-left">气温</span>
+              <span class="weather-item-right weather-item-small-right">14.1℃</span>
+            </li>
+            <li>
+              <span class="weather-item-left weather-item-mid-left">风力</span>
+              <span class="weather-item-right weather-item-mid-right">1级</span>
+            </li>
+            <li>
+              <span class="weather-item-left weather-item-big-left">风向</span>
+              <span class="weather-item-right weather-item-big-right">东南风</span>
+            </li>
+            <li>
+              <span class="weather-item-left weather-item-small-left">能见度</span>
+              <span class="weather-item-right weather-item-small-right">9.66KM</span>
+            </li>
+          </ul>
         </div>
         <!-- 通知公告 -->
         <div class="screen-item">
-          <img class="screen-item-bg" src="./assets/images/left_item_bg.png" alt />
-          <div class="screen-item-main">
-            <h3 class="screen-item-title">通知公告</h3>
-            <div class="warn-info">
-              <img class="notice-icon" src="./assets/images/notice_icon.png" alt />
-              <span>公告：</span>
-              <div class="warn-txt">
-                <p>关于建设平安武汉</p>
-                <p>支队持续推进暑期消防安全宣教活动</p>
-                <p>XX大队召开卫健系统消防安全培训会</p>
-                <p>XX大队到辖区沿街商铺开展消防宣传活动</p>
-              </div>
+          <h3 class="screen-item-title">通知公告</h3>
+          <div class="warn-info">
+            <img class="notice-icon" src="./assets/images/notice_icon.png" alt />
+            <span>公告：</span>
+            <div class="warn-txt">
+              <p>关于建设平安武汉</p>
+              <p>支队持续推进暑期消防安全宣教活动</p>
+              <p>XX大队召开卫健系统消防安全培训会</p>
+              <p>XX大队到辖区沿街商铺开展消防宣传活动</p>
             </div>
           </div>
         </div>
         <!-- 本周重点工作 -->
-        <div class="screen-item">
-          <img class="screen-item-bg" src="./assets/images/left_item_bg.png" alt />
-          <div class="screen-item-main">
-            <h3 class="screen-item-title">本周重点工作</h3>
-            <ul class="important-work">
-              <li>
-                <p>1.主题教育要走深走实</p>
-                <span>完成</span>
-              </li>
-              <li>
-                <p>2.主题教育要走深走实</p>
-                <span>进行中</span>
-              </li>
-              <li>
-                <p>3.主题教育要走深走实</p>
-                <span>完成</span>
-              </li>
-              <li>
-                <p>4.主题教育要走深走实</p>
-                <span>进行中</span>
-              </li>
-            </ul>
+        <div class="two-branch">
+          <div class="branch-item">
+            <h3 class="screen-item-title">教育训练计划</h3>
+            <div class="branch-cont">
+              <div class="branch-cont-title">
+                <span class="branch-time">时间</span>
+                <span class="branch-content">内容</span>
+              </div>
+              <ul class="branch-ul">
+                <li>
+                  <span class="branch-time">早上</span>
+                  <span class="branch-project">队容风纪检查</span>
+                </li>
+                <li>
+                  <span class="branch-time">上午</span>
+                  <span class="branch-project">1人3盘水带走接操</span>
+                </li>
+                <li>
+                  <span class="branch-time">下去</span>
+                  <span class="branch-project">思想政治教育</span>
+                </li>
+                <li>
+                  <span class="branch-time">晚上</span>
+                  <span class="branch-project">100m跑</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="branch-item">
+            <h3 class="screen-item-title">龙虎榜</h3>
+            <div class="branch-cont">
+              <div class="branch-cont-title branch-bang-title">
+                <span class="bang-subject">训练科目</span>
+                <span class="bang-name">姓名</span>
+                <span class="bang-score">成绩</span>
+              </div>
+              <ul class="bang-ul">
+                <li>
+                  <span class="bang-subject">单杠一练习</span>
+                  <span class="bang-name">沈伟</span>
+                  <span class="bang-score">32个</span>
+                </li>
+                <li>
+                  <span class="bang-subject">单杠二练习</span>
+                  <span class="bang-name">张旭</span>
+                  <span class="bang-score">25个</span>
+                </li>
+                <li>
+                  <span class="bang-subject">双杠一练习</span>
+                  <span class="bang-name">李坤</span>
+                  <span class="bang-score">35个</span>
+                </li>
+                <li>
+                  <span class="bang-subject">壶铃折返</span>
+                  <span class="bang-name">曹磊</span>
+                  <span class="bang-score">39"</span>
+                </li>
+                <li>
+                  <span class="bang-subject">200m负重跑</span>
+                  <span class="bang-name">华环</span>
+                  <span class="bang-score">36"</span>
+                </li>
+                <li>
+                  <span class="bang-subject">400m疏散物资</span>
+                  <span class="bang-name">李涛</span>
+                  <span class="bang-score">70"</span>
+                </li>
+                <li>
+                  <span class="bang-subject">3000m徒手跑</span>
+                  <span class="bang-name">宋奎</span>
+                  <span class="bang-score">11'05"</span>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
@@ -96,99 +142,89 @@
         <!-- xsheng 添加 srtat -->
         <div class="rigth">
           <!-- 今日警情信息strat -->
-          <div class="screen-item">
-            <img class="screen-item-bg" src="./assets/images/left_item_bg.png" alt />
-            <div class="screen-item-main">
-              <h3 class="screen-item-title">今日警情信息</h3>
-
-              <div class="num">接处警总量(8起)</div>
-              <div class="info">
-                <ul>
-                  <li>
-                    <div class="round1"></div>警情信息
-                  </li>
-                </ul>
-              </div>
-
-              <div class="info">
-                <ul class="tel_ul">
-                  <li>
-                    火灾扑救
-                    <div class="color_r mt">(1起)</div>
-                  </li>
-                  <li>
-                    抢险救援
-                    <div class="color_b mt">(2起)</div>
-                  </li>
-                  <li>
-                    社会救助
-                    <div class="color_y mt">(3起)</div>
-                  </li>
-                  <li>
-                    虚假报警
-                    <div class="color_g mt">(5起)</div>
-                  </li>
-                  <li>
-                    其他警情
-                    <div class="color_o mt">(0起)</div>
-                  </li>
-                </ul>
-              </div>
+          <div class="right-top screen-item">
+            <h3 class="screen-item-title">今日警情信息</h3>
+            <div class="num">接处警总量(8起)</div>
+            <div class="info info1">
+              <ul>
+                <li>
+                  <div class="round1"></div>警情信息
+                </li>
+              </ul>
+            </div>
+            <div class="info">
+              <ul class="tel_ul">
+                <li>
+                  火灾扑救
+                  <div class="color_r mt">(1起)</div>
+                </li>
+                <li>
+                  抢险救援
+                  <div class="color_b mt">(2起)</div>
+                </li>
+                <li>
+                  社会救助
+                  <div class="color_y mt">(3起)</div>
+                </li>
+                <li>
+                  虚假报警
+                  <div class="color_g mt">(5起)</div>
+                </li>
+                <li>
+                  其他警情
+                  <div class="color_o mt">(0起)</div>
+                </li>
+              </ul>
             </div>
           </div>
           <!-- 今日警情信息end -->
 
           <!-- 值勤车辆strat -->
-          <div class="screen-item">
-            <img class="screen-item-bg" src="./assets/images/left_item_bg.png" alt />
-            <div class="screen-item-main">
-              <h3 class="screen-item-title">值勤车辆</h3>
+          <div class="rigth-middle screen-item">
+            <h3 class="screen-item-title">值勤车辆</h3>
+            <div class="num1 zq-num1">
+              <ul class="zq_ul">
+                <li>
+                  <div class="round bj_g"></div>在位
+                </li>
+                <li>
+                  <div class="round bj_y"></div>出动
+                </li>
+                <li>
+                  <div class="round bj_r"></div>保修
+                </li>
+              </ul>
+            </div>
 
-              <div class="num1">
-                <ul class="zq_ul">
-                  <li>
-                    <div class="round bj_g"></div>在位
-                  </li>
-                  <li>
-                    <div class="round bj_y"></div>出动
-                  </li>
-                  <li>
-                    <div class="round bj_r"></div>保修
+            <div class="clear vehicle">
+              <vue-seamless-scroll
+                :data="listData"
+                class="seamless-warp"
+                :class-option="classOption"
+              >
+                <ul class="ve_ul">
+                  <li v-for="item in listData">
+                    <div class="list_bj">
+                      {{item.xfc}}
+                      <div v-if="item.status==1" class="round bj_y fr"></div>
+                      <div v-else-if="item.status==2" class="round bj_r fr"></div>
+                      <div v-else="item.status==3" class="round bj_g fr"></div>
+                    </div>
+                    <div class="list_text">
+                      <div class="mt2">发动机功率:{{item.gl}}</div>
+                      <div class="list_text2">起重重量:{{item.zl}}</div>
+                    </div>
                   </li>
                 </ul>
-              </div>
-
-              <div class="clear vehicle">
-                <vue-seamless-scroll
-                  :data="listData"
-                  class="seamless-warp"
-                  :class-option="classOption"
-                >
-                  <ul class="ve_ul">
-                    <li v-for="item in listData">
-                      <div class="list_bj">
-                        {{item.xfc}}
-                        <div v-if="item.status==1" class="round bj_y fr"></div>
-                        <div v-else-if="item.status==2" class="round bj_r fr"></div>
-                        <div v-else="item.status==3" class="round bj_g fr"></div>
-                      </div>
-                      <div class="list_text">
-                        <div class="mt2">发动机功率:{{item.gl}}</div>
-                        <div class="list_text">起重重量:{{item.zl}}</div>
-                      </div>
-                    </li>
-                  </ul>
-                </vue-seamless-scroll>
-              </div>
+              </vue-seamless-scroll>
             </div>
           </div>
           <!-- 值勤车辆end -->
 
           <!-- 值班信息strat -->
-          <div class="screen-item">
-            <img class="screen-item-bg" src="./assets/images/left_item_bg.png" alt />
-            <div class="screen-item-main">
-              <h3 class="screen-item-title">值班信息</h3>
+          <div class="right-bottom screen-item">
+            <h3 class="screen-item-title">值班信息</h3>
+            <div class="duty-main">
               <div class="duty">
                 <ul class="duty_ul">
                   <li class="size17">大队全勤指挥部:</li>
